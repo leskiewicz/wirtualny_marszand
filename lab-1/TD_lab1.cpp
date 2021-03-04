9518831a32f2f822c1ee918ad04c63ac2308abf1
Konrad Leśkiewicz <lk46576@zut.edu.pl>
2021-03-04 15:47:23 +0100

    lab-1

---
+++ lab-1/TD_lab1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int funkcja(int x) {
+	return x * x + 2 * x + 3;
+}
+
+int main()
+{
+	int tab[20];
+	for (int i = 0; i < 10; i++) {
+		tab[i] = funkcja(i);
+		std::cout << tab[i] << std::endl;
+	}
+}
