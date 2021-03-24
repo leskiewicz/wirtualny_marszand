4334df764390e098266c7747b7cbcdee236f0067
lk46576 <lk46576@zut.edu.pl>
2021-03-24 22:55:29 +0100

    dokończenie implementacji i porównanie dft z fft

--- lab-2/td_lab02.py
+++ lab-2/td_lab02.py
@@ -1,28 +1,33 @@
 import math as m
+import cmath as cm
+import time
+import numpy as np
 
-# tc = 10  # czas trwania
-# f = 2000  # częstotliwość
-# fs = 8000  # częstotliwość próbkowania
-# N = tc * fs  # liczba próbek na cały sygnał
-# ts = 1 / fs  # okres próbkowania
-# fi = 1
-# przebieg = []
-# sekundy = []
-#
-# for n in range(N):
-#     t = n * ts
-#     x = (1 - t) * m.sin(2 * m.pi * f * t + fi) * m.cos(4 * m.pi * t)
-#     przebieg.append(x)
-#     sekundy.append(t)
+def dft(x):
+    wyniki = []
+    N = len(x)
+    szereg = 0
+    for k in range(N):
+        for n in range(N):
+            szereg = szereg + x[n] * cm.exp(1j * ((2 * cm.pi * k * n) / N))
+        wyniki.append(szereg)
+    return wyniki
 
-x=[1,2,3,4,5,6,7]
-i = 1
-szereg = 0
-N = 7
-i=m.sqrt(-1)
-wyniki=[]
+x = []
+for i in range(10):
+    x.append(i)
 
-for k in range(N - 1):
-    for n in range(N - 1):
-        szereg = szereg + x[n] * m.pow(m.e, -i * ((2 * m.pi * k * n) / N))
-    wyniki.append(szereg)
+start = time.perf_counter()
+dft(x)
+koniec = time.perf_counter()
+print('Czas wynosi', f"{(koniec - start):.10f}", 'sekund')
+# print('Czas wynosi',koniec-start,'sekund')
+
+start = time.perf_counter()
+np.fft.fft(x)
+koniec = time.perf_counter()
+print('Czas wynosi', f"{(koniec - start):.10f}", 'sekund')
+# print('Czas wynosi',koniec-start,'sekund')
+
+print(dft(x))
+print(np.fft.fft(x))
