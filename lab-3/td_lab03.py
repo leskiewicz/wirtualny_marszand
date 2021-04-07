6068e8f5964bc0fdace80ff19daea6282c120508
lk46576 <lk46576@zut.edu.pl>
2021-04-08 00:08:34 +0200

    poprawione i dokonczone

--- lab-3/td_lab03.py
+++ lab-3/td_lab03.py
@@ -1,29 +1,29 @@
 import math as m
 import matplotlib.pyplot as plt
 
-def aplitudowo(x,t):
-    przebieg2=[]
-    for a in range(N):
-        ka=0.5
-        zA=(ka*x[a]+1)*m.cos(2*m.pi*fn*t)
-        przebieg2.append(zA)
-    return przebieg2
 
-# def mod_fazy(x,t):
-#     przebieg3=[]
-#     for a in range(len(x)):
-#         kp=2
-#         zP=m.cos(2*m.pi*fs*t+kp*x[a])
-#         przebieg3.append(zP)
-#     return przebieg3
-#
-# def mod_cz(x,t):
-#     przebieg4=[]
-#     for a in range(len(x)):
-#         kf=2
-#         zF=
-#         przebieg4.append(zF)
-#     return przebieg4
+def mod_apl(x, t, ka):
+    za = []
+    for i in range(N):
+        za.append((ka * x[i] + 1) * (m.cos(2 * m.pi * fn * t[i])))
+    plt.plot(czas, za)
+    plt.show()
+
+
+def mod_fazy(x, t, kp):
+    zp = []
+    for i in range(N):
+        zp.append(m.cos(2 * m.pi * fn * t[i] + kp * x[i]))
+    plt.plot(czas, zp)
+    plt.show()
+
+
+def mod_cz(x, t, kf):
+    zf = []
+    for i in range(N):
+        zf.append(m.cos(2 * m.pi * fn * t[i] + (kf / fm) * x[i]))
+    plt.plot(czas, zf)
+    plt.show()
 
 
 tc = 1  # czas trwania
@@ -31,20 +31,24 @@ fs = 8000  # częstotliwość próbkowania
 N = tc * fs  # liczba próbek na cały sygnał
 ts = 1 / fs  # okres próbkowania
 fi = 100
-
-fn=100
-fm=5
+fn = 100
+fm = 5
 
 przebieg = []
-sekundy = []
+czas = []
 for n in range(N):
-    t = n/fs
-    x = m.sin(2 * m.pi * fn * t)
-    przebieg.append(x)
-    sekundy.append(t)
-plt.plot(sekundy,przebieg)
-plt.show()
+    t = n / fs
+    przebieg.append(m.sin(2 * m.pi * fm * t))
+    czas.append(t)
 
-ampl=aplitudowo(przebieg,sekundy)
-plt.plot(sekundy,ampl)
-plt.show()
+mod_apl(przebieg, czas, 0.7)
+mod_fazy(przebieg, czas, 0.5)
+mod_cz(przebieg, czas, 0.2)
+
+mod_apl(przebieg, czas, 9.9)
+mod_fazy(przebieg, czas, 2.0)
+mod_cz(przebieg, czas, 2.5)
+
+mod_apl(przebieg, czas, 25.0)
+mod_fazy(przebieg, czas, 3 * m.pi)
+mod_cz(przebieg, czas, 4 * m.pi)
