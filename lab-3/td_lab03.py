9650c5e6aaaf88b20878d2cf46d70a2a23e267c9
lk46576 <lk46576@zut.edu.pl>
2021-04-14 13:09:14 +0200

    Zaktualizuj 'lab-3/td_lab03.py'

--- lab-3/td_lab03.py
+++ lab-3/td_lab03.py
@@ -8,8 +8,8 @@ def mod_apl(x, t, ka):
     za = []
     for i in range(N):
         za.append((ka * x[i] + 1) * (m.cos(2 * m.pi * fn * t[i])))
-    plt.plot(czas, za)
-    plt.show()
+    # plt.plot(czas, za)
+    # plt.show()
     return za
 
 
@@ -17,8 +17,8 @@ def mod_fazy(x, t, kp):
     zp = []
     for i in range(N):
         zp.append(m.cos(2 * m.pi * fn * t[i] + kp * x[i]))
-    plt.plot(czas, zp)
-    plt.show()
+    # plt.plot(czas, zp)
+    # plt.show()
     return zp
 
 
@@ -26,8 +26,8 @@ def mod_cz(x, t, kf):
     zf = []
     for i in range(N):
         zf.append(m.cos(2 * m.pi * fn * t[i] + (kf / fm) * x[i]))
-    plt.plot(czas, zf)
-    plt.show()
+    # plt.plot(czas, zf)
+    # plt.show()
     return zf
 
 
@@ -45,7 +45,6 @@ def dft(x):
 def moduly(zesp):
     modul = []
     N = len(zesp) / 2
-    # N = len(zesp)
     for i in range(int(N)):
         z = m.sqrt(zesp[i].real ** 2 + zesp[i].imag ** 2)
         modul.append(z)
@@ -64,12 +63,58 @@ def widmo(z):
     return Mprim
 
 
+def szerokosc_pasma(widmo, db):
+    maksymalna = max(widmo)
+    for i in range(len(widmo)):
+        widmo[i] = widmo[i] - maksymalna
+    for i in range(len(widmo)):
+        if widmo[i] >= (-db):
+            fmin = i
+            break
+    for i in range(len(widmo) - 1, 0, -1):
+        if widmo[i] >= (-db):
+            fmax = i
+            break
+    print('fmin:', fmin)
+    print('fmax:', fmax)
+    szerokosc = fmax - fmin
+    print('Szerokosc dla %ddB: %d' % (db, szerokosc))
+    # plt.plot(widmo)
+    # plt.show()
+
+
+def wywolywanie(ka, kp, kf):
+    apl = mod_apl(przebieg, czas, ka)
+    fazy = mod_fazy(przebieg, czas, kp)
+    cz = mod_cz(przebieg, czas, kf)
+    print('---------Modulacja amplitudy--------')
+    zesp = np.fft.fft(apl)
+    Z = moduly(zesp)
+    w = widmo(Z)
+    szerokosc_pasma(w, 3)
+    szerokosc_pasma(w, 6)
+    szerokosc_pasma(w, 12)
+    print('---------Modulacja fazy--------')
+    zesp = np.fft.fft(fazy)
+    Z = moduly(zesp)
+    w = widmo(Z)
+    szerokosc_pasma(w, 3)
+    szerokosc_pasma(w, 6)
+    szerokosc_pasma(w, 12)
+    print('---------Modulacja częstotliwości--------')
+    zesp = np.fft.fft(cz)
+    Z = moduly(zesp)
+    w = widmo(Z)
+    szerokosc_pasma(w, 3)
+    szerokosc_pasma(w, 6)
+    szerokosc_pasma(w, 12)
+
+
 tc = 1  # czas trwania
 fs = 800  # częstotliwość próbkowania
 N = tc * fs  # liczba próbek na cały sygnał
 ts = 1 / fs  # okres próbkowania
-# fi = 100
-fn = 150  # częstotliwość nośna
+fn = 200  # częstotliwość nośna
 fm = 5  # częstotliwość sygnału informacyjnego
 
 przebieg = []
@@ -79,53 +124,12 @@ for n in range(N):
     przebieg.append(m.sin(2 * m.pi * fm * t))
     czas.append(t)
 
-ka = 0.7
-kp = 0.5
-kf = 0.2
-apl = mod_apl(przebieg, czas, ka)
-fazy = mod_fazy(przebieg, czas, kp)
-cz = mod_cz(przebieg, czas, kf)
-
-zesp = np.fft.fft(apl)
-Z = moduly(zesp)
-widmo(Z)
-zesp = np.fft.fft(fazy)
-Z = moduly(zesp)
-widmo(Z)
-zesp = np.fft.fft(cz)
-Z = moduly(zesp)
-widmo(Z)
-
-ka = 9.9
-kp = 3.0
-kf = 2.5
-apl = mod_apl(przebieg, czas, ka)
-fazy = mod_fazy(przebieg, czas, kp)
-cz = mod_cz(przebieg, czas, kf)
-
-zesp = np.fft.fft(apl)
-Z = moduly(zesp)
-widmo(Z)
-zesp = np.fft.fft(fazy)
-Z = moduly(zesp)
-widmo(Z)
-zesp = np.fft.fft(cz)
-Z = moduly(zesp)
-widmo(Z)
-
-ka = 25.0
-kp = 3 * m.pi
-kf = 4 * m.pi
-apl = mod_apl(przebieg, czas, ka)
-fazy = mod_fazy(przebieg, czas, kp)
-cz = mod_cz(przebieg, czas, kf)
-
-zesp = np.fft.fft(apl)
-Z = moduly(zesp)
-widmo(Z)
-zesp = np.fft.fft(fazy)
-Z = moduly(zesp)
-widmo(Z)
-zesp = np.fft.fft(cz)
-Z = moduly(zesp)
-widmo(Z)
+print('===========Przykład 1===============')
+ka, kp, kf = 0.7, 0.5, 0.2
+wywolywanie(ka, kp, kf)
+print('===========Przykład 2===============')
+ka, kp, kf = 9.9, 3.0, 2.5
+wywolywanie(ka, kp, kf)
+print('===========Przykład 3===============')
+ka, kp, kf = 25.0, 3 * m.pi, 4 * m.pi
+wywolywanie(ka, kp, kf)
