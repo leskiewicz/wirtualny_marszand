892db402c75ed57d0b854b740b2bff15304152f9
lk46576 <lk46576@zut.edu.pl>
2021-04-22 15:44:58 +0200

    Zaktualizuj 'lab-4/td-lab04.py'

--- lab-4/td-lab04.py
+++ lab-4/td-lab04.py
@@ -2,9 +2,23 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
+
+def sygnal(string):
+    n = 0
+    sygnal = []
+    TMP_TB = 0
+    for k in range(N):
+        TMP_TB = TMP_TB + ts
+        if TMP_TB >= tb:
+            n += 1
+            TMP_TB = 0
+        sygnal.append(string[n])
+    return sygnal
+
+
 def mod_apl(x):
     za = []
-    for i in range(len(x)):
+    for i in range(N):
         t = i * ts
         if x[i] == '0':
             za.append(A1 * (m.sin(2 * m.pi * fn * t)))
@@ -17,7 +31,7 @@ def mod_apl(x):
 
 def mod_fazy(x):
     zp = []
-    for i in range(len(x)):
+    for i in range(N):
         t = i * ts
         if x[i] == '0':
             zp.append(m.sin(2 * m.pi * fn * t))
@@ -30,7 +44,7 @@ def mod_fazy(x):
 
 def mod_cz(x):
     zf = []
-    for i in range(len(x)):
+    for i in range(N):
         t = i * ts
         if x[i] == '0':
             zf.append(m.sin(2 * m.pi * fn1 * t))
@@ -41,30 +55,68 @@ def mod_cz(x):
     return zf
 
 
+def moduly(zesp):
+    modul = []
+    N = len(zesp) / 2
+    # N = len(zesp)
+    for i in range(int(N)):
+        z = m.sqrt(zesp[i].real ** 2 + zesp[i].imag ** 2)
+        modul.append(z)
+    # plt.plot(modul)
+    # plt.show()
+    return modul
+
+
+def widmo(z):
+    Mprim = []
+    for k in range(len(z)):
+        M = 10 * m.log10(z[k])
+        Mprim.append(M)
+    plt.plot(Mprim)
+    plt.show()
+    return Mprim
+
+
 def konwertuj(string):
     # return ''.join(format(ord(x), 'b') for x in string)
     return ''.join(format(i, 'b') for i in bytearray(string, encoding='utf8'))
 
-napis = '''ascutdayvgtzsyrdtjugyfsdfgvbhujdxcfvgbhnjdxcfvgbhwzesxdrcftgvh
-xdrcftvgqazwxsecdrvftgbyhtxhddrhcftvuygklihkenbfryiwekcuocbqwucxdfcgvhdfg
-hddrhcftvuygklihkenbfryiwekcuocbqwucerrewwwwwwerwrsedtfghvxdfcgdrtfvygcft'''
-# napis = 'krotki napis zeby mozna bylo zmodulowac te sygnaly'
+
+napis = '''acdd'''
 b = konwertuj(napis)
-# print(b)
+print(b)
 
-tc = 400  # czas trwania
-fs = 6000  # częstotliwość próbkowania
-# N = tc * fs  # liczba próbek na cały sygnał
+A1 = 1
+A2 = 2
+W = 2
+
+tc = 1  # czas trwania
+tb = tc / len(b)  # czas trwania pojedynczego bitu
+fs = 800  # częstotliwość próbkowania
 ts = 1 / fs  # okres próbkowania
+# N = tc*fs  # liczba próbek na cały sygnał
+N = m.ceil(tc / ts)
 
-Tb = tc/len(b)  # czas trwania pojedynczego bitu
-A1 = 2
-A2 = 3
-W = 4
-fn = W * Tb ** (-1)  # częstotliwość nośna
-fn1 = (W + 1) / Tb
-fn2 = (W + 2) / Tb
+fn = W * tb ** (-1)  # częstotliwość nośna
+fn1 = (W + 1) / tb
+fn2 = (W + 2) / tb
 
-apl = mod_apl(b)
-fazy = mod_fazy(b)
-cz = mod_cz(b)
+sygnal = sygnal(b)
+plt.plot(sygnal)
+plt.show()
+
+apl = mod_apl(sygnal)
+
+zesp = np.fft.fft(apl)
+Z = moduly(zesp)
+w = widmo(Z)
+
+fazy = mod_fazy(sygnal)
+zesp = np.fft.fft(fazy)
+Z = moduly(zesp)
+w = widmo(Z)
+
+cz = mod_cz(sygnal)
+zesp = np.fft.fft(cz)
+Z = moduly(zesp)
+w = widmo(Z)
