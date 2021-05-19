dc50b59c8fd676953dacc43dfa2e30fc34aec9fd
lk46576 <lk46576@zut.edu.pl>
2021-05-20 00:44:46 +0200

    Zaktualizuj 'lab-6/td-lab06.py'

--- lab-6/td-lab06.py
+++ lab-6/td-lab06.py
@@ -2,38 +2,48 @@ import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
+
 def koder(b):
-    print(b)
-    w = []
-    w.append = (b[0] + b[1] + b[3]) % 2
-    w.append=b[0]
-    w.append = (b[0] + b[2] + b[3]) % 2
-    w.append = (b[1] + b[2] + b[3]) % 2
-    # w=p1+p2+b[0]+p3+b[1]+b[2]+b[3]
-    w.append=b[1]
-    w.append=b[2]
-    w.append=b[3]
-    print(w)
+    w = [None] * n
+    w[0] = (b[0] + b[1] + b[3]) % 2
+    w[1] = (b[0] + b[2] + b[3]) % 2
+    w[2] = b[0]
+    w[3] = (b[1] + b[2] + b[3]) % 2
+    w[4] = b[1]
+    w[5] = b[2]
+    w[6] = b[3]
     return w
 
 
 def dekoder(b):
-    print(b)
+        x1p = (b[2] + b[4] + b[6]) % 2
+        x2p = (b[2] + b[5] + b[6]) % 2
+        x4p = (b[4] + b[5] + b[6]) % 2
+        x1 = (b[0] + x1p) % 2
+        x2 = (b[1] + x2p) % 2
+        x4 = (b[3] + x4p) % 2
+        S=x1*1+x2*2+x4*2**2
+        print('S =',S)
 
+        if S:
+            b[S-1]=int(not(b[S-1]))
+            return b, 'tak'
+        else:
+            return b, 'nie'
 
-# def konwertuj(string):
-#     return ''.join(format(i, 'b') for i in bytearray(string, encoding='utf8'))
-# napis= konwertuj('a')
-# napis=list(napis)
-# napis = [int(i) for i in napis]
 
 n = 7
 k = 4
-m = n - k
 
 napis = ("1101")
 napis = [int(i) for i in napis]
-# enc = encode(napis)
-# print(enc)
+
 kod = koder(napis)
-print(kod)
+print(kod,'\n')
+
+dekod = dekoder(kod)
+print('Błąd:',dekod[1],'\n')
+
+kod[4]=int(not(kod[4]))
+dekod = dekoder(kod)
+print('Błąd:',dekod[1])
